5ca1f8b49c7fee6ecc38e470e54cb9c918e69afc
Veronika Kravchenko <aniregma@gmail.com>
2022-10-26 23:49:13 +0300

    ColorPicker to hooks

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,42 +1,72 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Wrap, Title, Text, ButtonWrap, Button } from './ColorPicker.styled';
 
-export class ColorPicker extends Component {
-  state = {
-    activeOptionIdx: 0,
-  };
+export function ColorPicker({ options }) {
+  const [activeOptionIdx, setActiveOptionIdx] = useState(0);
+  const { label } = options[activeOptionIdx];
 
-  setActiveIdx = index => {
-    this.setState({ activeOptionIdx: index });
-  };
-
-  render() {
-    const { activeOptionIdx } = this.state;
-    const { options } = this.props;
-    const { label } = options[activeOptionIdx];
-
-    return (
-      <Wrap>
-        <Title>Color Picker</Title>
-        <Text>Selected is {label}</Text>
-        <ButtonWrap>
-          {this.props.options.map(({ label, color }, index) => (
-            <Button
-              key={label}
-              style={{
-                backgroundColor: color,
-                border:
-                  index === this.state.activeOptionIdx
-                    ? '2px solid black'
-                    : 'none',
-              }}
-              onClick={() => this.setActiveIdx(index)}
-            ></Button>
-          ))}
-        </ButtonWrap>
-      </Wrap>
-    );
-  }
+  return (
+    <Wrap>
+      <Title>Color Picker</Title>
+      <Text>Selected is {label}</Text>
+      <ButtonWrap>
+        {options.map(({ label, color }, index) => (
+          <Button
+            key={label}
+            style={{
+              backgroundColor: color,
+              border: index === activeOptionIdx ? '2px solid black' : 'none',
+            }}
+            onClick={() => setActiveOptionIdx(index)}
+          ></Button>
+        ))}
+      </ButtonWrap>
+    </Wrap>
+  );
 }
+
+//анонимка для обновления части стейта
+//onClick={() => setActiveOptionIdx(index)}
+//
+//const { label } = options[activeOptionIdx];
+// options[activeOptionIdx].label;
+// class OldColorPicker extends Component {
+//   state = {
+//     activeOptionIdx: 0,
+//   };
+
+//   setActiveIdx = index => {
+//     this.setState({ activeOptionIdx: index });
+//   };
+
+//   render() {
+//     const { activeOptionIdx } = this.state;
+//     const { options } = this.props;
+//     //  by idx get ref on active obj
+//     const { label } = options[activeOptionIdx];
+
+//     return (
+//       <Wrap>
+//         <Title>Color Picker</Title>
+//         <Text>Selected is {label}</Text>
+//         <ButtonWrap>
+//           {this.props.options.map(({ label, color }, index) => (
+//             <Button
+//               key={label}
+//               style={{
+//                 backgroundColor: color,
+//                 border:
+//                   index === this.state.activeOptionIdx
+//                     ? '2px solid black'
+//                     : 'none',
+//               }}
+//               onClick={() => this.setActiveIdx(index)}
+//             ></Button>
+//           ))}
+//         </ButtonWrap>
+//       </Wrap>
+//     );
+//   }
+// }
 
 // замыкание
